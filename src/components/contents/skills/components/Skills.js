51be4a42795af72efda29a0d896f8368aaf80df8
Ciruela-Xcyba17her <lovetkg3584@gmail.com>
2020-03-08 23:46:15 +0900

    added general skill data to Skills page

--- src/components/contents/skills/components/Skills.js
+++ src/components/contents/skills/components/Skills.js
@@ -1,4 +1,5 @@
 import React from "react";
+import generalSkillDataAll from "./general-skilldata"
 import ctfSkillDataAll from "./ctf-skilldata";
 import programmingSkillDataAll from "./programming-skilldata"
 import "./skills.css";
@@ -10,6 +11,34 @@ class Skills extends React.Component {
             <div >
                 <h1>Skills</h1>
 
+                {/*----- General skills section -----*/}
+                <h2>General Skills</h2>
+                <p>
+                    General skills, what I experienced more than once.
+                </p>
+
+                <ul className="skill">
+                    {/* list general skills */}
+                    {generalSkillDataAll.map((generalSkillData) => {
+                        return (
+                            <li>
+                                {/* print general skill name */}
+                                {generalSkillData.skill}
+
+                                {/* list general skill detail */}
+                                <ul className="skill-detail">
+                                    {generalSkillData.detail.map((generalSkillDetail) => {
+                                        return (
+                                            <li>{generalSkillDetail}</li>
+                                        );
+                                    })}
+                                </ul>
+                            </li>
+                        );
+                    })}
+                </ul>
+
+                {/*----- CTF skills section -----*/}
                 <h2>CTF</h2>
                 <p>
                     Capture The Flag - Competition that requires knowledge and skills of cybersecurity.
@@ -17,21 +46,28 @@ class Skills extends React.Component {
                     In this section, I'll introduce what I can do in each categories.
                 </p>
 
-                {/*Unreadable...*/}
                 <div className="skills-ctf-container">
+
+                    {/* print CTF skill explanation boxes separated by category */}
                     {ctfSkillDataAll.map((ctfSkillDataForEachCategory) => {
                         return (
                             <div className="skills-ctf-grid" key={ctfSkillDataForEachCategory.bgColor} style={{ backgroundColor: ctfSkillDataForEachCategory.bgColor }}>
+
+                                {/* print CTF category */}
                                 <h3>{ctfSkillDataForEachCategory.category}</h3>
+
+                                {/* list skills in each category */}
                                 <ul className="skill">
-                                    {ctfSkillDataForEachCategory.skills.map((skills) => {
+                                    {ctfSkillDataForEachCategory.skills.map((ctfSkillForEachCategory) => {
                                         return (
                                             <li>
-                                                {skills.skill}
+
+                                                {/* list skill detail */}
+                                                {ctfSkillForEachCategory.skill}
                                                 <ul className="skill-detail">
-                                                    {skills.detail.map((detail) => {
+                                                    {ctfSkillForEachCategory.detail.map((ctfSkillDetail) => {
                                                         return (
-                                                            <li>{detail}</li>
+                                                            <li>{ctfSkillDetail}</li>
                                                         );
                                                     })}
                                                 </ul>
@@ -44,12 +80,15 @@ class Skills extends React.Component {
                     })}
                 </div>
 
+                {/*----- Programming skills section -----*/}
                 <h2>Programming</h2>
                 <p>
                     Programming languages which I wrote more than once. As you can see from my GitHub page (see "Links" page),
                     I don't have any expensive works. I will be glad if you don't look down me.
                 </p>
+
                 <div className="skills-programming-container">
+                    {/* list programming skills and detail of each skill */}
                     {programmingSkillDataAll.map((programmingSkillData) => {
                         return (
                             <>
---
+++ src/components/contents/skills/components/general-skilldata.js
@@ -0,0 +1,42 @@
+const generalSkillDataAll = [
+    {
+        "skill": "static routing (netplan)",
+        "detail": [
+        ]
+    },
+    {
+        "skill": "Firewall settings (ufw)",
+        "detail": [
+        ]
+    },
+    {
+        "skill": "DNS server setting (BIND9)",
+        "detail": [
+            "dig command to check settings"
+        ]
+    },
+    {
+        "skill": "NFS server setting (nfs-kernel-server)",
+        "detail": [
+        ]
+    },
+    {
+        "skill": "LDAP client/server setting",
+        "detail": [
+        ]
+    },
+    {
+        "skill": "SMTP server(Postfix), POP/IMAP server(Dovecot) setting",
+        "detail": [
+        ]
+    },
+    {
+        "skill": "Docker",
+        "detail": [
+            "HTTPS connection setting(http-portal version 1) between nginx server and user",
+            "developped web service(flask) with database(MySQL)"
+        ]
+    }
+];
+
+export default generalSkillDataAll;

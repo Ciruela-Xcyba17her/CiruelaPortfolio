7fb2d1e675c9c8fbffec276ccbe82dac247ca32e
Ciruela-Xcyba17her <lovetkg3584@gmail.com>
2020-03-14 08:19:09 +0900

    moved links data to linksdata.js

--- src/components/contents/links/components/Links.js
+++ src/components/contents/links/components/Links.js
@@ -1,64 +1,21 @@
 import React from "react";
+import LinksBoxDataAll from "./linksdata"
 import { LinksButton, LinksBox } from "./links-components";
 
 class Links extends React.Component {
     render() {
-        const linksBoxConstants = [
-            {
-                serviceName: "Speaker Deck",
-                description: "I uploaded some writeups of CTF as slides.",
-                href: "https://speakerdeck.com/xcyba17her",
-                bgColor: "#202060",
-                addition: "",
-            },
-            {
-                serviceName: "Hack The Box",
-                description: "An online service to improve penetration testing skills and other cybersecurity skills. I own no machines now...",
-                href: "https://www.hackthebox.eu/profile/206153",
-                bgColor: "#208020",
-                addition: "",
-            },
-            {
-                serviceName: "GitHub",
-                description: "Cheap programs I developed, and some writeups of CTF.",
-                href: "https://github.com/Ciruela-Xcyba17her",
-                bgColor: "#404040",
-                addition: "",
-            },
-            {
-                serviceName: "Twitter",
-                description: "My Twitter account.",
-                href: "https://twitter.com/__Xcyba17her_",
-                bgColor: "#5050c0",
-                addition: "",
-            },
-            {
-                serviceName: "Hatena Blog",
-                description: "I post mainly CTF writeups.",
-                href: "https://verliezer93764.hatenablog.jp/",
-                bgColor: "#606060",
-                addition: "",
-            },
-            {
-                serviceName: "Root Me",
-                description: "An web service to train specific skills of vulnerability attack on each category of CTF.",
-                href: "https://www.root-me.org/Ciruela?inc=info&lang=en",
-                bgColor: "#907010",
-                addition: "",
-            },
-        ];
         return (
             <div>
                 <h1>Links</h1>
                 <p>Most of my works are written in Japanese.</p>
                 <div className="links-container">
-                    {linksBoxConstants.map((linksBoxConstant) => {
+                    {LinksBoxDataAll.map((LinksBoxData) => {
                         return (
-                            <LinksBox bgColor={linksBoxConstant.bgColor} key={linksBoxConstant.serviceName}>
-                                <h2>{linksBoxConstant.serviceName}</h2>
-                                <p>{linksBoxConstant.description}</p>
-                                <p>{linksBoxConstant.addition}</p>
-                                <LinksButton href={linksBoxConstant.href} key={linksBoxConstant.serviceName} value={"Move to " + linksBoxConstant.serviceName + " account"} />
+                            <LinksBox bgColor={LinksBoxData.bgColor} key={LinksBoxData.serviceName}>
+                                <h2>{LinksBoxData.serviceName}</h2>
+                                <p>{LinksBoxData.description}</p>
+                                <p>{LinksBoxData.addition}</p>
+                                <LinksButton href={LinksBoxData.href} key={LinksBoxData.serviceName} value={"Move to " + LinksBoxData.serviceName + " account"} />
                             </LinksBox>
                         );
                     })}
---
+++ src/components/contents/links/components/linksdata.js
@@ -0,0 +1,46 @@
+const linksBoxDataAll = [
+    {
+        serviceName: "Speaker Deck",
+        description: "I uploaded some writeups of CTF as slides.",
+        href: "https://speakerdeck.com/xcyba17her",
+        bgColor: "#202060",
+        addition: "",
+    },
+    {
+        serviceName: "Hack The Box",
+        description: "An online service to improve penetration testing skills and other cybersecurity skills. I own no machines now...",
+        href: "https://www.hackthebox.eu/profile/206153",
+        bgColor: "#208020",
+        addition: "",
+    },
+    {
+        serviceName: "GitHub",
+        description: "Cheap programs I developed, and some writeups of CTF.",
+        href: "https://github.com/Ciruela-Xcyba17her",
+        bgColor: "#404040",
+        addition: "",
+    },
+    {
+        serviceName: "Twitter",
+        description: "My Twitter account.",
+        href: "https://twitter.com/__Xcyba17her_",
+        bgColor: "#5050c0",
+        addition: "",
+    },
+    {
+        serviceName: "Hatena Blog",
+        description: "I post mainly CTF writeups.",
+        href: "https://verliezer93764.hatenablog.jp/",
+        bgColor: "#606060",
+        addition: "",
+    },
+    {
+        serviceName: "Root Me",
+        description: "An web service to train specific skills of vulnerability attack on each category of CTF.",
+        href: "https://www.root-me.org/Ciruela?inc=info&lang=en",
+        bgColor: "#907010",
+        addition: "",
+    },
+];
+
+export default linksBoxDataAll;

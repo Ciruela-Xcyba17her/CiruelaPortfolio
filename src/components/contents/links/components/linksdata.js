dd1c21bc05ae5b061084ec5c3d0d848da0e85900
Ciruela-Xcyba17her <lovetkg3584@gmail.com>
2021-04-05 23:50:58 +0900

    added a link for nlp100 answers

--- src/components/contents/links/components/linksdata.js
+++ src/components/contents/links/components/linksdata.js
@@ -7,6 +7,27 @@ const linksBoxDataAll = [
         addition: "",
     },
     {
+        serviceName: "NLP100 exercise answers",
+        description: "My answers for natural language processing (NLP) 100 exercise.",
+        href: "https://colab.research.google.com/drive/1UwYgnTHxtWH0xch3lIaUYTSQpRQ7WImI?usp=sharing",
+        bgColor: "#202060",
+        addition: "#907010",
+    },
+    {
+        serviceName: "Hatena Blog",
+        description: "I post mainly CTF writeups.",
+        href: "https://verliezer93764.hatenablog.jp/",
+        bgColor: "#606060",
+        addition: "",
+    },
+    {
+        serviceName: "Twitter",
+        description: "My Twitter account.",
+        href: "https://twitter.com/__Xcyba17her_",
+        bgColor: "#5050c0",
+        addition: "",
+    },
+    {
         serviceName: "Hack The Box",
         description: "An online service to improve penetration testing skills and other cybersecurity skills. I own no machines now...",
         href: "https://www.hackthebox.eu/profile/206153",
@@ -21,24 +42,10 @@ const linksBoxDataAll = [
         addition: "",
     },
     {
-        serviceName: "Twitter",
-        description: "My Twitter account.",
-        href: "https://twitter.com/__Xcyba17her_",
-        bgColor: "#5050c0",
-        addition: "",
-    },
-    {
-        serviceName: "Hatena Blog",
-        description: "I post mainly CTF writeups.",
-        href: "https://verliezer93764.hatenablog.jp/",
-        bgColor: "#606060",
-        addition: "",
-    },
-    {
         serviceName: "Root Me",
         description: "An web service to train specific skills of vulnerability attack on each category of CTF.",
         href: "https://www.root-me.org/Ciruela?inc=info&lang=en",
-        bgColor: "#907010",
+        bgColor: "#ff8c00",
         addition: "",
     },
 ];
